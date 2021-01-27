5b55aad5c13a987dc59b9037a4be6fc7bc993832
Marvin Figueredo <marvin.elder7@gmail.com>
2021-01-27 18:12:22 -0300

    Publish settings

--- components/WorkComponent/WorkComponent.js
+++ components/WorkComponent/WorkComponent.js
@@ -4,8 +4,8 @@ import { getAllExperiences, getWorkContent } from "../../api/workExperienceApi";
 
 export default function WorkComponent() {
   const router = useRouter();
-  const [experiences, setExperiences] = useState([]);
   const [pageContent, setPageContent] = useState({});
+  const [experiences, setExperiences] = useState(undefined);
 
   useEffect(() => {
     const content = getWorkContent(router.locale || "es");
@@ -21,24 +21,25 @@ export default function WorkComponent() {
           <h1>{pageContent.title}</h1>
           <div className="experience">
             <ul>
-              {experiences.map((item, index) => (
-                <li key={index}>
-                  <div className="year">
-                    <span className="to">{item.yearStart}</span>
-                    <span className="from">{item.yearEnd}</span>
-                  </div>
-                  <div className="timeline">
-                    <span className="line-before"></span>
+              {experiences &&
+                experiences.map((item, index) => (
+                  <li key={index}>
+                    <div className="year">
+                      <span className="to">{item.yearStart}</span>
+                      <span className="from">{item.yearEnd}</span>
+                    </div>
+                    <div className="timeline">
+                      <span className="line-before"></span>
 
-                    <span className="line-after"></span>
-                  </div>
-                  <div className="experience__content">
-                    <h4>{item.title}</h4>
-                    <h5>{item.company}</h5>
-                    <p className="experience__info">{item.description}</p>
-                  </div>
-                </li>
-              ))}
+                      <span className="line-after"></span>
+                    </div>
+                    <div className="experience__content">
+                      <h4>{item.title}</h4>
+                      <h5>{item.company}</h5>
+                      <p className="experience__info">{item.description}</p>
+                    </div>
+                  </li>
+                ))}
             </ul>
           </div>
         </div>
--- pages/work.js
+++ pages/work.js
@@ -5,7 +5,7 @@ export default function work() {
   return (
     <>
       <Seo title="Marvin - Works" />
-      <WorkComponent />
+      {/* <WorkComponent /> */}
     </>
   );
 }
