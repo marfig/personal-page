b39be3e773333feb30c9e3cce202e8dc5f9b55b0
marfig <marvin.elder7@gmail.com>
2024-11-02 11:12:55 -0300

    Update workExperienceApi.js

--- api/workExperienceApi.js
+++ api/workExperienceApi.js
@@ -29,8 +29,8 @@ export function getAllExperiences(lan) {
       company: "Investor Casa de Bolsa S.A.",
       description: `Responsible for the development and maintenance of the information systems of the company. 
             Technologies: .Net MVC, .Net Core, SQL Server, Postgres SQL`,
-      yearStart: 2014,
-      yearEnd: 2021,
+      yearStart: '2014',
+      yearEnd: 'present',
       lan: "en",
     },
     {
@@ -38,8 +38,8 @@ export function getAllExperiences(lan) {
       company: "Taxare S.A. y Roshka",
       description: `Desarrollo de Aplicación Web para procesamiento Impositivo. Tecnologías:
             Java Spring, Paython Django, Postgres SQL.`,
-      yearStart: 2013,
-      yearEnd: 2014,
+      yearStart: '2013',
+      yearEnd: '2014',
       lan: "es",
     },
     {
@@ -47,8 +47,8 @@ export function getAllExperiences(lan) {
       company: "Taxare S.A. y Roshka",
       description: `Web Application Development for Tax Processing. Technologies:
             Java Spring, Paython Django, Postgres SQL.`,
-      yearStart: 2013,
-      yearEnd: 2014,
+      yearStart: '2013',
+      yearEnd: '2014',
       lan: "en",
     },
     {
@@ -56,8 +56,8 @@ export function getAllExperiences(lan) {
       company: "IDTEKNOLOGY",
       description: `Desarrollo de Home Banking para empresa del sistema financiero.
             Tecnologías: .Net MVC, SQL Server, Oracle.`,
-      yearStart: 2012,
-      yearEnd: 2013,
+      yearStart: '2012',
+      yearEnd: '2013',
       lan: "es",
     },
     {
@@ -65,8 +65,8 @@ export function getAllExperiences(lan) {
       company: "IDTEKNOLOGY",
       description: `Development of Home Banking for financial system company.
       Technologies: .Net MVC, SQL Server, Oracle.`,
-      yearStart: 2012,
-      yearEnd: 2013,
+      yearStart: '2012',
+      yearEnd: '2013',
       lan: "en",
     },
     {
@@ -74,16 +74,16 @@ export function getAllExperiences(lan) {
       company: "Nucleo S.A.",
       description: `Desarrollo y mantenimiento de sistemas internos de la
             comapañía. Tecnologías: C#, WPF, SQL Server, Oracle.`,
-      yearStart: 2011,
-      yearEnd: 2012,
+      yearStart: '2011',
+      yearEnd: '2012',
       lan: "es",
     },
     {
       title: "Software Developer",
       company: "Nucleo S.A.",
       description: `Development and maintenance of internal company systems. Technologies: C#, WPF, SQL Server, Oracle.`,
-      yearStart: 2011,
-      yearEnd: 2012,
+      yearStart: '2011',
+      yearEnd: '2012',
       lan: "en",
     },
     {
@@ -91,16 +91,16 @@ export function getAllExperiences(lan) {
       company: "INAVAN Paraguay",
       description: `Desarrollo de diversos sistemas para clientes locales y del
             exterior. Tecnologías: Asp Net Web Forms, PHP, SQL Server`,
-      yearStart: 2008,
-      yearEnd: 2011,
+      yearStart: '2008',
+      yearEnd: '2011',
       lan: "es",
     },
     {
       title: "Software Developer",
       company: "INAVAN Paraguay",
       description: `Development of various systems for local and external clients. Technologies: Asp Net Web Forms, PHP, SQL Server`,
-      yearStart: 2008,
-      yearEnd: 2011,
+      yearStart: '2008',
+      yearEnd: '2011',
       lan: "en",
     },
   ];
